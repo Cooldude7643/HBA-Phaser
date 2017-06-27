92802e453d6bcf33042e79e64738d2fc5ef7f712
Cooldude7643 <naturewilderness80845@gmail.com>
2017-06-26 14:55:38 -1000

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,27 +5,59 @@ function init(){
 function preload(){
 	game.load.image('background', 'images/background.png');
 	game.load.json('level:1', 'data/level01.json');
-    //spawn platform sprites
     game.load.image('ground', 'images/ground.png');
     game.load.image('grass:8x1', 'images/grass_8x1.png');
     game.load.image('grass:6x1', 'images/grass_6x1.png');
     game.load.image('grass:4x1', 'images/grass_4x1.png');
     game.load.image('grass:2x1', 'images/grass_2x1.png');
     game.load.image('grass:1x1', 'images/grass_1x1.png');
+    game.load.image('grass:1x1', 'images/grass_1x1.png');
+    game.load.image('hero', 'images/hero_stopped.png');
 };
 
 function create(){
-	game.add.image(0, 0, 'background');
+    game.add.image(0, 0, 'background');
   loadLevel(this.game.cache.getJSON('level:1'));
-}
+  leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+  rightKey = game.imput.keyboard.addKey(Phaser.Keyboard.RIGHT);
+};
+
 function update(){
-	
+	handleInput();
 }
+
+function spawnPlatform(platform) {
+    game.add.sprite(platform.x, platform.y, platform.image);
+};
 
 function loadLevel(data) {
 	console.log(data);
+    data.platforms.forEach(spawnPlatform, this);
+    spawnCharacters({hero: data.hero});
 };
 
+function spawnCharacters (data) {
+    hero = game.add.sprite(data.hero.x, data.hero.y, 'hero');
+    hero.anchor.set(0.5, 0.5);
+    hero = game.add.sprite(data.hero.x, data.hero.y, 'hero');
+    hero.anchor.set(0.5, 0.5);
+    hero = game.add.sprite(data.hero.x, data.hero.y, 'hero');
+    hero.anchor.set(0.5, 0.5);
+    game.physics.enable(hero);
+};
 
+function move(direction){
+    hero.body.velocity.x = direction * 200;
+};
+
+function handInput(){
+    if (leftKey.isDown) { // move hero left
+        move(-1);
+    }
+    else if (rightKey.isDown) {
+    move(1) // move hero right
+        // ? - pass a parameter to the move function to move hero right
+    }
+}
 //Create a game state
 var game = new Phaser.Game(960, 600, Phaser.AUTO, 'game', {init: init, preload: preload, create: create, update: update});

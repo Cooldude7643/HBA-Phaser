49df228a6d33ec439efe21f6356b7cf9371233df
Cooldude7643 <naturewilderness80845@gmail.com>
2017-06-28 14:51:08 -1000

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ function init(){
 
 function preload(){
 	game.load.image('background', 'images/background.png');
-	game.load.json('level:1', 'data/level01.json');
+	game.load.audio('sfx:jump', 'audio/jump.wav');
+    game.load.json('level:1', 'data/level01.json');
     game.load.image('ground', 'images/ground.png');
     game.load.image('grass:8x1', 'images/grass_8x1.png');
     game.load.image('grass:6x1', 'images/grass_6x1.png');
@@ -12,19 +13,29 @@ function preload(){
     game.load.image('grass:2x1', 'images/grass_2x1.png');
     game.load.image('grass:1x1', 'images/grass_1x1.png');
     game.load.image('grass:1x1', 'images/grass_1x1.png');
-    game.load.image('hero', 'images/hero_stopped.png');
+      game.load.audio('sfx:jump', 'audio/jump.wav');
+      game.load.image('hero', 'images/hero_stopped.png');
+       //game.load.i
+    game.load.audio('sfx:jump', 'audio/jump.wav');
+    game.load.spritesheet('coin', 'images/coin_animated.png', 22, 22);
 };
 
 function create(){
+     sfxJump = game.add.audio('sfx:jump');
     game.add.image(0, 0, 'background');
   loadLevel(this.game.cache.getJSON('level:1'));
   leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
   rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
-}
+    upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+    upKey.onDown.add(function(){
+        jump();
+    })
+};
+
 
 function update(){
-	handleInput();
-   
+    handleInput();
+    handleCollisions();
 }
  function handleInput(){
     if (leftKey.isDown) { // move hero left
@@ -34,8 +45,9 @@ function update(){
         move(1);
     }
     else { //stop
+        move(0);
     }
-}
+};
 function spawnPlatform(platform) {
     game.add.sprite(platform.x, platform.y, platform.image);
     var sprite = platforms.create(platform.x, platform.y, platform.image);
@@ -43,13 +55,18 @@ function spawnPlatform(platform) {
     game.physics.enable(sprite);
     sprite.body.allowGravity = false;
     sprite.body.immovable = true;
+    game.add.sprite(platform.x, platform.y, platform.image);
+    var sprite = platforms.create(platform.x, platform.y, platform.image);
+    
 };
 
 function loadLevel(data) {
-	console.log(data);
+	platforms = game.add.group();
+    console.log(data);
     data.platforms.forEach(spawnPlatform, this);
     spawnCharacters({hero: data.hero});
-    game.physics.arcade.gravity.y = 1200;
+    game.physics.arcade.gravity.y = 1200;  // ..
+        data.coins.forEach(spawnCoin, this);
 };
 
 function spawnCharacters (data) {
@@ -57,7 +74,7 @@ function spawnCharacters (data) {
     hero.anchor.set(0.5, 0.5);
     //Make the main character use the physics engine for movement
     game.physics.enable(hero);
-    hero.body
+    hero.body.collideWorldBounds = true;
 };
 
 function move(direction){
@@ -82,5 +99,20 @@ function handInput(){
     }
 };
 
+function handleCollisions(){
+   game.physics.arcade.collide(hero, platforms);
+};
+
+function jump(){
+        var canJump = hero.body.touching.down;
+    //Ensures hero is on the ground or on a platform
+    if (canJump) {
+        hero.body.velocity.y = -600;
+        sfxJump.play();
+    }
+   
+    return canJump;
+   
+};
 //Create a game state
 var game = new Phaser.Game(960, 600, Phaser.AUTO, 'game', {init: init, preload: preload, create: create, update: update});
